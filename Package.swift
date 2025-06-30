e929589c75c0058fb5b07b6a301c639685d5598d
Suzuki, Kenta <129563320+AP3-GitHub@users.noreply.github.com>
2025-06-30 17:20:13 +0900

    feat: add support for Unified ID (update dependencies) (#4)
    * add: create .gitignore file
    
    * fix: replace repositories with local paths (temporary)
    
    * chore: replace local paths with GitHub repositories

--- Package.swift
+++ Package.swift
@@ -15,17 +15,17 @@ let package = Package(
     dependencies: [
 		.package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.5.3"),
-		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-iso18013-data-transfer.git", exact: "0.2.9"),
-		.package(url: "https://github.com/dentsusoken/tw-lib-ios-wallet-storage", branch: "main"),
+        .package(url: "https://github.com/dentsusoken/tw-lib-ios-iso18013-data-transfer.git", branch: "feat-UnifiedID"),
+        .package(url: "https://github.com/dentsusoken/tw-lib-ios-wallet-storage.git", branch: "feat-UnifiedID"),
 		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-siop-openid4vp-swift.git", exact: "0.1.1"),
-		.package(url: "https://github.com/dentsusoken/tw-lib-ios-openid4vci-swift", branch: "main"),
+        .package(url: "https://github.com/dentsusoken/tw-lib-ios-openid4vci-swift.git", branch: "feat-UnifiedID"),
 	],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "EudiWalletKit", dependencies: [
-		    	.product(name: "MdocDataTransfer18013", package: "eudi-lib-ios-iso18013-data-transfer"),
+				.product(name: "MdocDataTransfer18013", package: "tw-lib-ios-iso18013-data-transfer"),
 				.product(name: "WalletStorage", package: "tw-lib-ios-wallet-storage"),
 				.product(name: "SiopOpenID4VP", package: "eudi-lib-ios-siop-openid4vp-swift"),
 				.product(name: "OpenID4VCI", package: "tw-lib-ios-openid4vci-swift"),
